ee606fb855d2d9203648ef86d2f28818fef70316
leonardo brito <leonardonunesbrito@gmail.com>
2024-09-11 11:53:04 -0300

    List resizeble (mocked data)

--- medicamentapp/lib/features/medicamentos/screens/medicamento_list_screen.dart
+++ medicamentapp/lib/features/medicamentos/screens/medicamento_list_screen.dart
@@ -9,6 +9,153 @@ class MedicamentoListScreen extends StatefulWidget {
   const MedicamentoListScreen({super.key});
 
   @override
+  _MedicamentoListScreenState createState() => _MedicamentoListScreenState();
+}
+
+class _MedicamentoListScreenState extends State<MedicamentoListScreen> {
+  final MedicamentoApiService _apiService = MedicamentoApiService();
+  final List<Medicamento> _medicamentos = [];
+  int _page = 1;
+  late int _limit;
+  bool _isLoading = false;
+  bool _hasMoreItems = true;
+  String _searchQuery = '';
+  final ScrollController _scrollController = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController.addListener(_onScroll);
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    // Calcula o limite após o contexto estar disponível
+    _limit = _calculateLimit();
+    _fetchMedicamentos();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _fetchMedicamentos() async {
+    if (_isLoading || !_hasMoreItems) return;
+
+    setState(() {
+      _isLoading = true;
+    });
+
+    try {
+      final response =
+          await _apiService.fetchMedicamentos(_searchQuery, _limit, _page);
+      final novosMedicamentos = response['medicamentos'] as List<Medicamento>;
+      final pagination = response['pagination'];
+
+      if (!mounted) return;
+
+      setState(() {
+        _medicamentos.addAll(novosMedicamentos);
+        _page++;
+        _hasMoreItems = pagination['hasMoreItems'];
+      });
+      print('Total de medicamentos recebidos: ${_medicamentos.length}');
+    } catch (e) {
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Erro ao carregar medicamentos: $e')),
+        );
+      }
+    } finally {
+      if (mounted) {
+        setState(() {
+          _isLoading = false;
+        });
+      }
+    }
+  }
+
+  void _onScroll() {
+    final offsetParaChamadaApi = MediaQuery.of(context).size.height * 0.2;
+
+    if (_scrollController.position.pixels >=
+            _scrollController.position.maxScrollExtent - offsetParaChamadaApi &&
+        !_isLoading &&
+        _hasMoreItems) {
+      _fetchMedicamentos();
+    }
+  }
+
+  void _filterMedicamentos(String query) {
+    if (query.isEmpty) {
+      setState(() {
+        _searchQuery = '';
+        _medicamentos.clear();
+        _page = 1;
+        _hasMoreItems = true;
+        _limit = _calculateLimit(); // Recalcula o limite com base na tela atual
+      });
+    } else if (query.length >= 3) {
+      setState(() {
+        _searchQuery = query;
+        _medicamentos.clear();
+        _page = 1;
+        _hasMoreItems = true;
+        _limit = _calculateLimit(); // Recalcula o limite com base na tela atual
+      });
+    }
+    _fetchMedicamentos();
+  }
+
+  // Calcula o número de itens a serem carregados com base na altura da tela
+  int _calculateLimit() {
+    final screenHeight = MediaQuery.of(context).size.height;
+    const tileHeight = 40.0; // Altura aproximada de cada item da lista
+    return (screenHeight / tileHeight).floor(); // Calcula quantos cabem na tela
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: MedicamentoAppBar(onSearch: _filterMedicamentos),
+      body: Container(
+        color: const Color.fromARGB(223, 33, 59, 105),
+        child: ListView.builder(
+          controller: _scrollController,
+          itemCount: _medicamentos.length + (_isLoading ? 1 : 0),
+          itemBuilder: (context, index) {
+            if (index == _medicamentos.length) {
+              return const LoadingIndicator();
+            }
+
+            final medicamento = _medicamentos[index];
+            return MedicamentoTile(
+              medicamento: medicamento,
+              onTap: () {},
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+
+
+/* import 'package:flutter/material.dart';
+import 'package:medicamentapp/core/widgets/medicamento_app_bar.dart';
+import 'package:medicamentapp/core/widgets/medicamento_tile.dart';
+import 'package:medicamentapp/shared/widgets/loading_indicator.dart';
+import 'package:medicamentapp/core/services/medicamento_api_service.dart';
+import 'package:medicamentapp/features/medicamentos/models/medicamento.dart';
+
+class MedicamentoListScreen extends StatefulWidget {
+  const MedicamentoListScreen({super.key});
+
+  @override
   // ignore: library_private_types_in_public_api
   _MedicamentoListScreenState createState() => _MedicamentoListScreenState();
 }
@@ -131,3 +278,4 @@ class _MedicamentoListScreenState extends State<MedicamentoListScreen> {
     );
   }
 }
+ */
